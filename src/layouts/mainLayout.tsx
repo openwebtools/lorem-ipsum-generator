9507fe8689aacec1500cbeb5a3ca37493e3785ab
Vivek S <vivekthottathil@gmail.com>
2020-05-01 23:59:43 +0530

    unused removal

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import { Layout } from 'antd';
 import CSS from 'csstype';
-import AppToolbar from '../components/appToolbar';
 
 const layoutStyle: CSS.Properties = {
     minHeight: '100%',
     minWidth: '100%'
-}
-
-const headerStyle: CSS.Properties = {
-    boxShadow: '0 2px 8px rgba(0, 0, 0, 0.15)',
-    justifyContent: 'center',
-    display: 'flex'
 }
 
 const footerStyle: CSS.Properties = {
